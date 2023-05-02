01bae32e809c040e2723372b8e66a3e2e88e6768
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-05-02 11:33:29 +0530

    added script to run test suite

---
+++ run_test_suite.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+
+def main(test_path, test_pattern):
+    print(('Discovering tests in : {}'.format(test_path)))
+    suite = unittest.TestLoader().discover(test_path, test_pattern)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    root_path = os.path.dirname(__file__)
+    test_path = os.path.join(root_path, 'tests/')
+    test_pattern = 'test_*'
+    main(test_path, test_pattern)
