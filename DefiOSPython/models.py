ca98907c7aef7d1c0e3d6c5872a29a41ccd29e74
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2023-05-25 01:27:20 +0530

    changed rewardee from User referencing to address of rewardee

--- DefiOSPython/models.py
+++ DefiOSPython/models.py
@@ -152,7 +152,7 @@ class Issues(Document):
     issue_stake_token_url = URLField()
     issue_prs = EmbeddedDocumentListField(IssuePRs)
     issue_tags = ListField(StringField())
-    rewardee = ReferenceField(Users)
+    rewardee = StringFeild()
 
     def parse_to_json(self):
         issue_json = self.to_mongo().to_dict()
