6f339c7c33fe209e387bde0240d2f42ef1f15773
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-06-21 05:43:30 +0000

    corrected bug in swap page api

--- DefiOSPython/swap.py
+++ DefiOSPython/swap.py
@@ -55,7 +55,7 @@ def fetch_token_list(token):
     if not isAuthorized:
         return resp
     try:
-        tokens = Token.objects.only(
+        tokens = Token.objects(token_new=True).only(
             "token_image_url", "token_symbol", "token_spl_addr"
         ).all()
         messages = []
